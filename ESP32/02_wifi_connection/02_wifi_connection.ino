e1f8d5e36a42ac1202e4e36a784baa5a3844c57b
Sheethal26-marker <sheethal.ramdas26@gmail.com>
2026-08-01 09:58:19 +0530

    Add WiFi connection setup for ESP32

---
+++ ESP32/02_wifi_connection/02_wifi_connection.ino
@@ -0,0 +1,29 @@
+#include <WiFi.h>
+
+const char *ssid = "wifi_username";
+const char *password = "password";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi Connected");
+
+  Serial.print("ESP32 IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
